a55e63a8ddce5346ff1ac897c11585946f29abe3
xsnowholy <495904127@qq.com>
2023-09-03 20:28:05 +0800

    feat: 新增登录逻辑

--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { List, Input, Button } from 'zarm';
 import CustomIcon from '@/components/CustomIcon';
 import s from './style.module.less';
+import { post } from '@/utils'
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const onLogin = async () => {
+    setLoading(true);
+    const res = await post('/api/user/login', {username, password}).catch(() => {
+      setLoading(false);
+    });
+    if(res && res.data) {
+      localStorage.setItem('token', res.data.token);
+      // 跳转到首页
+      navigate('/');
+    }
+    setLoading(false);
+  };
+
   return (
     <div>
       <h1>Login</h1>
@@ -18,7 +39,14 @@ const Login = () => {
             }
             description="最少4个字符，包含大小写英文字母和数字"
           >
-            <Input label="账号" placeholder="请输入您的账号" />
+            <Input
+              label="账号"
+              placeholder="请输入您的账号"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
           </List.Item>
           <List.Item
             prefix={
@@ -30,10 +58,18 @@ const Login = () => {
             }
             description="最少8个字符，包含大小写英文字母、数字和字符"
           >
-            <Input label="密码" type="password" placeholder="请输入您的密码" />
+            <Input
+              label="密码"
+              type="password"
+              placeholder="请输入您的密码"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
           </List.Item>
         </List>
-        <Button className={s.btn} block theme="primary">
+        <Button onClick={onLogin} loading={loading} className={s.btn} block theme="primary">
           登录
         </Button>
       </div>

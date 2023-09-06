b8b4a9f2e6c7b88bf2b8983dc1ed752d92823bac
xsnowholy <495904127@qq.com>
2023-09-06 10:45:12 +0800

    feat: 头部组件样式调整

--- src/components/CustomIcon/index.jsx
+++ src/components/CustomIcon/index.jsx
@@ -1,7 +1,7 @@
 import { Icon } from 'zarm';
 
 const IconFont = Icon.createFromIconfont(
-  '//at.alicdn.com/t/c/font_4232054_3koep22tl3w.js',
+  '//at.alicdn.com/t/c/font_4232054_ude2c6kh1n.js',
 );
 
 export default IconFont
--- src/container/Home/Header.jsx
+++ src/container/Home/Header.jsx
@@ -1,22 +1,29 @@
 import s from './Header.module.less';
 import ProTypes from 'prop-types';
+import CustomIcon from '@/components/CustomIcon'
 
 const Header = ({ totalIncome, totalExpense, type, date,  onSelectType, onSelectDate }) => {
   return (
     <div className={s.headerBody}>
       <div className={s.top}>
         <div className={s.statistics}>
-          <span className={s.key} >总支出:</span>
-          <span className={s.value}>￥{totalIncome}</span>
-        </div>
-        <div className={s.statistics}>
           <span className={s.key}>总支出:</span>
           <span className={s.value}>￥{totalExpense}</span>
         </div>
+        <div className={s.statistics}>
+          <span className={s.key} >总收入:</span>
+          <span className={s.value}>￥{totalIncome}</span>
+        </div>
       </div>
       <div className={s.bottom}>
-        <div onClick={onSelectType} className={s.selectBtn}>{type || '全部类型' }</div>
-        <div onClick={onSelectDate} className={s.selectBtn}>{date}</div>
+        <div onClick={onSelectType} className={s.selectBtn}>
+          <span>{type || '全部类型' }</span>
+          <CustomIcon type='down' />
+        </div>
+        <div onClick={onSelectDate} className={s.selectBtn}>
+          <span>{date}</span>
+          <CustomIcon type='down' />
+        </div>
       </div>
     </div>
   );

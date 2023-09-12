9a734b5e567791d7fb33ec2c0d5e0d809e9e50ad
xsnowholy <495904127@qq.com>
2023-09-12 22:12:46 +0800

    feat: 新增退出登录功能、个人中心页面样式优化

--- src/container/User/index.jsx
+++ src/container/User/index.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react'
-import { Image, Button, List, Toast } from 'zarm';
+import { useState, useEffect } from 'react';
+import { Image, Button, List, Toast, Modal } from 'zarm';
 import s from './style.module.less';
-import { get } from '@/utils'
+import { get } from '@/utils';
+import CustomIcon from '@/components/CustomIcon';
 
 const User = () => {
   const [userInfo, setUserInfo] = useState({});
 
   const getUserInfo = async () => {
-    const res = await get('/api/user/get_userinfo')
-    if(res && res.code === 200) {
-      setUserInfo(res.data)
+    const res = await get('/api/user/get_userinfo');
+    if (res && res.code === 200) {
+      setUserInfo(res.data);
     } else {
-      Toast.show(res.msg || '获取用户信息失败')
+      Toast.show(res.msg || '获取用户信息失败');
     }
   };
 
@@ -19,15 +20,27 @@ const User = () => {
     getUserInfo();
   }, []);
 
+  const onLogout = () => {
+    Modal.confirm({
+      title: '提示',
+      content: '确定退出登录吗？',
+      onConfirm: async () => {
+        // 退出登录
+        localStorage.removeItem('token');
+        window.location.href = '/login';
+      },
+    });
+  };
+
   return (
     <div className={s.userWrap}>
       <div className={s.header}>
         <div className={s.info}>
           <div className={s.nick}>
             <span>昵称:</span>
-            <span className={s.nickText}>{ userInfo.username }</span>
+            <span className={s.nickText}>{userInfo.username}</span>
           </div>
-          <div className={s.signature}>{ userInfo.signature }</div>
+          <div className={s.signature}>{userInfo.signature}</div>
         </div>
         <div className={s.imgBox}>
           <Image src={userInfo.avatar} shape="circle" />
@@ -35,13 +48,30 @@ const User = () => {
       </div>
       <div className={s.actionsList}>
         <List>
-          <List.Item hasArrow title="用户信息修改" onClick={() => {}} />
-          <List.Item hasArrow title="重置密码" onClick={() => {}} />
-          <List.Item hasArrow title="关于我们" onClick={() => {}} />
+          <List.Item
+            hasArrow
+            title="用户信息修改"
+            prefix={<CustomIcon className={s.customIcon} type="edit" />}
+            onClick={() => {}}
+          />
+          <List.Item
+            hasArrow
+            title="重置密码"
+            prefix={<CustomIcon className={s.passwordIcon} type="password" />}
+            onClick={() => {}}
+          />
+          <List.Item
+            hasArrow
+            title="关于我们"
+            prefix={<CustomIcon className={s.userIcon} type="user" />}
+            onClick={() => {}}
+          />
         </List>
       </div>
       <div className={s.btnWrap}>
-        <Button shadow block theme="danger">退出登录</Button>
+        <Button onClick={onLogout} shadow block theme="danger">
+          退出登录
+        </Button>
       </div>
     </div>
   );

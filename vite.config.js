3a20f40f64115746279bbe52bc89deac4bdfbe30
xsnowholy <495904127@qq.com>
2023-09-18 11:17:09 +0800

    refactor: 调整测试环境 api 接口地址

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:7001',
+        target: 'http://192.168.31.213:7001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
